612c880b6e5814bbb1641d953388e888f14044f0
Raoyk <253300160@qq.com>
2021-03-20 18:20:38 +0800

    upload

--- src/MetaProgram/KCallable.kt
+++ src/MetaProgram/KCallable.kt
@@ -3,7 +3,15 @@ package MetaProgram
 import kotlin.reflect.KMutableProperty
 import kotlin.reflect.full.memberProperties
 
-data class Person (val name:String ,val age:Int ,var address:String)
+data class Person (val name:String ,val age:Int ,var address:String){
+    fun friendsName ():List<String>{
+        return listOf("Yison","Jilen")
+    }
+    fun <A> get (a:A):A{
+        return a
+    }
+}
+
 fun KMutablePropertyShow(){
     val p=Person("极跑科技",8,"HangZhou")
     val props=p::class.memberProperties
@@ -16,6 +24,39 @@ fun KMutablePropertyShow(){
     println(p.address)
 }
 
+//KParameter
+fun KParameterShow(){
+    val p=Person("极跑科技",8,"hangzhou")
+    for(c in Person::class.members){
+        print("${c.name} ->")
+        for(p in c.parameters){
+            print("${p.type}"+"--")
+        }
+        println()
+    }
+}
+
+fun KTypeParameterShow(){
+    for (c in Person::class.members){
+        if(c.name.equals("get")) {
+            println(c.typeParameters)
+        }
+    }
+    val list = listOf<String>("How")
+    println(list::class.typeParameters)
+}
 fun main(args: Array<String>) {
     KMutablePropertyShow()
+    println()
+    KParameterShow()
+    println()
+
+    //KType
+    Person::class.members.forEach{
+        println("${it.name}->${it.returnType.classifier}")
+    }
+    println()
+
+    KTypeParameterShow()
+    println()
 }
