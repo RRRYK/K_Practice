c8c33522975767c61cfab32bea19f0d2d9c768d9
Raoyk <253300160@qq.com>
2021-03-23 17:38:18 +0800

    upload

--- src/DesignPatterns/ObserverMode.kt
+++ src/DesignPatterns/ObserverMode.kt
@@ -1,21 +1,22 @@
 package DesignPatterns
+
 import kotlin.properties.Delegates
 
-interface StockUpdateListener{
-    fun onRise(price:Int)
-    fun onFall(price:Int)
+interface StockUpdateListener {
+    fun onRise(price: Int)
+    fun onFall(price: Int)
 }
 
-class StockUpdate{
-    var listener= mutableSetOf<StockUpdateListener>()
-    var price:Int by Delegates.observable(0){_,old,new->
+class StockUpdate {
+    var listener = mutableSetOf<StockUpdateListener>()
+    var price: Int by Delegates.observable(0) { _, old, new ->
         listener.forEach {
-            if(new>old)it.onRise(price) else it.onFall(price)
+            if (new > old) it.onRise(price) else it.onFall(price)
         }
     }
 }
 
-class StockDisplay:StockUpdateListener{
+class StockDisplay : StockUpdateListener {
     override fun onRise(price: Int) {
         println("The latest stock price has risen to ${price}.")
     }
@@ -26,9 +27,9 @@ class StockDisplay:StockUpdateListener{
 }
 
 fun main(args: Array<String>) {
-    val su=StockUpdate()
-    val sd=StockDisplay()
+    val su = StockUpdate()
+    val sd = StockDisplay()
     su.listener.add(sd)
-    su.price=100
-    su.price=98
+    su.price = 100
+    su.price = 98
 }
